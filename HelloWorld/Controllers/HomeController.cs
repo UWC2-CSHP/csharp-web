590a284a97d952b24a499da6898b6f9997bbe328
sabet <sabet@cstructor.com>
2023-04-20 21:07:20 -0700

    End of Session 1

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        // ADD ACTION
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        // ADD ACTION: POST
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            // validation action to force the user to enter their name
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        public string? Phone { get; set; }
+
+        // add email
+        public string? Email { get; set; }
+
+        public bool? WillAttend { get; set; }
+    }
+}
